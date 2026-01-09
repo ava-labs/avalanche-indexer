cd86c362ebbe2dfefa8f1ae5c2ae864687057c75
Sayan kar <sayan.kar@avalabs.org>
2026-01-09 16:50:22 +0530

    chore: remove schema registry and add block and transaction fields

--- go.mod
+++ go.mod
@@ -19,7 +19,7 @@ require (
 	github.com/ava-labs/avalanchego v1.14.0 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.5 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
@@ -30,11 +30,10 @@ require (
 	github.com/cockroachdb/pebble v0.0.0-20230928194634-aa077af62593 // indirect
 	github.com/cockroachdb/redact v1.1.3 // indirect
 	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
-	github.com/consensys/bavard v0.1.13 // indirect
-	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/consensys/gnark-crypto v0.18.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20231025140028-3c0104f4b233 // indirect
-	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
@@ -61,7 +60,6 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
@@ -73,8 +71,8 @@ require (
 	github.com/prometheus/procfs v0.16.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/supranational/blst v0.3.14 // indirect
@@ -110,5 +108,7 @@ require (
 	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	rsc.io/tmplfunc v0.0.3 // indirect
 )
+
+// Fix ambiguous import for cloud.google.com/go/compute/metadata
+replace cloud.google.com/go => cloud.google.com/go v0.115.0
--- pkg/slidingwindow/worker/coreth.go
+++ pkg/slidingwindow/worker/coreth.go
@@ -4,34 +4,28 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-	"time"
-
-	"github.com/confluentinc/confluent-kafka-go/v2/schemaregistry"
-	"github.com/confluentinc/confluent-kafka-go/v2/schemaregistry/serde"
 
 	"github.com/ava-labs/avalanche-indexer/pkg/kafka"
-	"github.com/ava-labs/avalanche-indexer/pkg/types"
-	"github.com/ava-labs/coreth/plugin/evm/customethclient"
+	"github.com/ava-labs/avalanche-indexer/pkg/types/coreth"
+	evmclient "github.com/ava-labs/coreth/plugin/evm/customethclient"
 	"github.com/ava-labs/coreth/plugin/evm/customtypes"
 	"github.com/ava-labs/coreth/rpc"
-	libevmtypes "github.com/ava-labs/libevm/core/types"
+	"go.uber.org/zap"
 )
 
 type CorethWorker struct {
-	client         *customethclient.Client
-	producer       *kafka.Producer
-	schemaRegistry schemaregistry.Client
-	serializer     serde.Serializer
-	topic          string
+	client   *evmclient.Client
+	producer *kafka.Producer
+	topic    string
+	log      *zap.SugaredLogger
 }
 
 func NewCorethWorker(
 	ctx context.Context,
 	url string,
 	producer *kafka.Producer,
-	schemaRegistry schemaregistry.Client,
-	serializer serde.Serializer,
 	topic string,
+	log *zap.SugaredLogger,
 ) (*CorethWorker, error) {
 	customtypes.Register()
 
@@ -40,11 +34,10 @@ func NewCorethWorker(
 		return nil, fmt.Errorf("dial coreth rpc: %w", err)
 	}
 	return &CorethWorker{
-		client:         customethclient.New(c),
-		producer:       producer,
-		schemaRegistry: schemaRegistry,
-		serializer:     serializer,
-		topic:          topic,
+		client:   evmclient.New(c),
+		producer: producer,
+		topic:    topic,
+		log:      log,
 	}, nil
 }
 
@@ -55,8 +48,12 @@ func (w *CorethWorker) Process(ctx context.Context, height uint64) error {
 		return fmt.Errorf("fetch block %d: %w", height, err)
 	}
 
-	corethBlock := createCorethBlock(block)
-	bytes, err := w.serializer.Serialize(w.topic, &corethBlock)
+	corethBlock, err := coreth.BlockFromLibevm(block)
+	if err != nil {
+		return fmt.Errorf("convert block %d: %w", height, err)
+	}
+
+	bytes, err := corethBlock.Marshal()
 	if err != nil {
 		return fmt.Errorf("serialize block %d: %w", height, err)
 	}
@@ -71,59 +68,11 @@ func (w *CorethWorker) Process(ctx context.Context, height uint64) error {
 		return fmt.Errorf("failed to produce block %d: %w", height, err)
 	}
 
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-time.After(300 * time.Millisecond):
-	}
-
-	fmt.Printf("processed block %d | hash=%s | txs=%d\n",
-		height, block.Hash().Hex(), len(block.Transactions()))
+	w.log.Debugw("processed block",
+		"height", height,
+		"hash", block.Hash().Hex(),
+		"txs", len(block.Transactions()),
+	)
 
 	return nil
-}
-
-func createCorethBlock(block *libevmtypes.Block) *types.CorethBlock {
-	return &types.CorethBlock{
-		Number:        block.Number(),
-		GasLimit:      block.GasLimit(),
-		GasUsed:       block.GasUsed(),
-		Difficulty:    block.Difficulty(),
-		Time:          block.Time(),
-		NumberU64:     block.NumberU64(),
-		MixDigest:     block.MixDigest(),
-		Nonce:         block.Nonce(),
-		Bloom:         block.Bloom(),
-		Coinbase:      block.Coinbase(),
-		Root:          block.Root(),
-		ParentHash:    block.ParentHash(),
-		TxHash:        block.TxHash(),
-		ReceiptHash:   block.ReceiptHash(),
-		UncleHash:     block.UncleHash(),
-		Extra:         block.Extra(),
-		BaseFee:       block.BaseFee(),
-		BeaconRoot:    block.BeaconRoot(),
-		ExcessBlobGas: block.ExcessBlobGas(),
-		BlobGasUsed:   block.BlobGasUsed(),
-		Withdrawals:   block.Withdrawals(),
-		Transactions:  createCorethTransactions(block.Transactions()),
-	}
-}
-
-func createCorethTransactions(transactions []*libevmtypes.Transaction) []*types.CorethTransaction {
-	corethTransactions := make([]*types.CorethTransaction, len(transactions))
-	for i, transaction := range transactions {
-		corethTransactions[i] = &types.CorethTransaction{
-			Hash:                 transaction.Hash(),
-			To:                   transaction.To(),
-			MsgGasPrice:          transaction.GasPrice(),
-			MaxPriorityFeePerGas: transaction.GasTipCap(),
-			MaxFeePerGas:         transaction.GasFeeCap(),
-			Value:                transaction.Value(),
-			Type:                 transaction.Type(),
-			Nonce:                transaction.Nonce(),
-			Data:                 transaction.Data(),
-		}
-	}
-	return corethTransactions
 }
