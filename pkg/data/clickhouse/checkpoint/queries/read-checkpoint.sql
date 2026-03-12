c7e0acd41046efe31ca601b9754937cbb8ca386b
Sayan kar <sayan.kar@avalabs.org>
2026-03-12 22:34:49 +0530

    chore: fix column order in read query for checkpoints (#103)
    * chore: fix column order in read query for checkpoints
    
    * chore: refactor uts
    
    * fix: lint issues

--- pkg/data/clickhouse/checkpoint/queries/read-checkpoint.sql
+++ pkg/data/clickhouse/checkpoint/queries/read-checkpoint.sql
@@ -1,1 +1,1 @@
-SELECT * FROM `%s`.`%s` WHERE chain_id = ? AND mode = ? ORDER BY timestamp DESC LIMIT 1
+SELECT chain_id, mode, lowest_unprocessed_block, timestamp FROM `%s`.`%s` WHERE chain_id = ? AND mode = ? ORDER BY timestamp DESC LIMIT 1
