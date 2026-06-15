065b7ae8501cf76e106757e018b86743ade65b64
artemanosov <aranosov@protonmail.com>
2026-06-15 11:15:21 -0400

    Create queries for icm tables

---
+++ pkg/data/clickhouse/icmrepo/queries/fee_info_events/batch-insert-fee-info-events.sql
@@ -0,0 +1,5 @@
+INSERT INTO `%s`.`%s` (
+    blockchain_id, evm_chain_id, block_number, block_time, tx_hash, tx_index, log_index,
+    contract_address, message_id, destination_blockchain_id,
+    fee_token_address, additional_fee_amount
+)
---
+++ pkg/data/clickhouse/icmrepo/queries/messages/write-partial-execution-failed.sql
@@ -0,0 +1,4 @@
+INSERT INTO `%s`.`%s` (
+    source_blockchain_id, destination_blockchain_id, message_id,
+    last_execution_failed_time
+) VALUES (?, ?, ?, ?)
